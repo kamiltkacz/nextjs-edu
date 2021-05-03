cebc51c6eb143b8123a408c47fa9bde602251097
Kamil Tkacz <kamiltkacz@gmail.com>
2021-05-03 17:18:07 -0300

    sample commit

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Nuxt App</title>
+        <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
