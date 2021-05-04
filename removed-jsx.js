1b79701a218c64e2d3399d396beb7c207c925d4b
Kamil Tkacz <kamiltkacz@gmail.com>
2021-05-03 21:32:57 -0300

    remove file

---
+++ removed-jsx.js
@@ -0,0 +1,80 @@
+
+/* content */
+
+<h1 className="title">
+          Learn<a href="https://nextjs.org">Next.js!</a>
+        </h1>
+
+        <p className="description">
+          Get started by editing <code>pages/index.js</code>
+        </p>
+ <a href="https://nextjs.org/learn" className="card">
+ <h3>Learn &rarr;</h3>
+ <p>Learn about Next.js in an interactive course with quizzes!</p>
+</a>
+
+<a
+ href="https://github.com/vercel/next.js/tree/master/examples"
+ className="card"
+>
+ <h3>Examples &rarr;</h3>
+ <p>Discover and deploy boilerplate example Next.js projects.</p>
+</a>
+
+<a
+ href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+ className="card"
+>
+ <h3>Deploy &rarr;</h3>
+ <p>
+   Instantly deploy your Next.js site to a public URL with Vercel.
+ </p>
+</a>
+
+
+
+
+CSSSS
+
+footer {
+    width: 100%;
+    height: 100px;
+    border-top: 1px solid #eaeaea;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  footer img {
+    margin-left: 0.5rem;
+  }
+
+  footer a {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .card:hover,
+.card:focus,
+.card:active {
+  color: #0070f3;
+  border-color: #0070f3;
+}
+
+
+
+
+
+
+
+<footer>
+<a
+  href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+  target="_blank"
+  rel="noopener noreferrer"
+>
+  Powered by{' '}
+  <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
+</a>
+</footer>
