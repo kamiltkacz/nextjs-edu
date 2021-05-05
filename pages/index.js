a7ac0abf39b4e79779fe2b1385342270dd7832da
Kamil Tkacz <kamiltkacz@gmail.com>
2021-05-05 18:11:48 -0300

    logo + no parent divs

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
+
 
 
 
@@ -12,20 +12,21 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>EDUICK</title>
       </Head>
-      <header className="headerStyle">
-       <p className="logo-first-letter"></p>
-       {/* <div className="logo-group-letters">
-       <p className="logo-second-letter"></p>
-       <p className="logo-third-letter"></p>
-       <p className="logo-fourth-letter"></p>
-       </div> */}
-       <p className="p-works">How it works</p>
-       <p className="p-about">About Us</p>
+
+       <div className="logo-first-letter"></div>
+       <div className="logo-second-letter"></div>
+       <div className="logo-third-letter"></div>
+       <div className="logo-fourth-letter"></div>
+       <div className="logo-fifth-letter"></div>
+       <div className="logo-sixth-letter"></div>
+       <div className="logo-seventh-letter"></div>
+
+       <div className="p-works">How it works</div>
+       <div className="p-about">About Us</div>
        <button className="btn-get-started">Get Started</button>
-      </header>
+
 
 
 
@@ -35,54 +36,60 @@ export default function Home() {
         <div className="grid">
         <div className="shape-left-1"></div>
         <div className="shape-left-2"></div>
+
+
+
+
+
+        <img className="girl-student" src="/images/student.jpg"/>
+
+            {/* <div className="dotted-detail"> */}
+
+{/* <div className="vector-row-1"> */}
+<div className="vector-1-1"></div>
+<div className="vector-1-2"></div>
+<div className="vector-1-3"></div>
+<div className="vector-1-4"></div>
+<div className="vector-1-5"></div>
+<div className="vector-1-6"></div>
+{/* </div><br /> */}
+
+
+{/* <div className="vector-row-2"> */}
+<div className="vector-2-1"></div>
+<div className="vector-2-2"></div>
+<div className="vector-2-3"></div>
+<div className="vector-2-4"></div>
+<div className="vector-2-5"></div>
+<div className="vector-2-6"></div>
+{/* </div><br /> */}
+{/* <div className="vector-row-3"> */}
+<div className="vector-3-1"></div>
+<div className="vector-3-2"></div>
+<div className="vector-3-3"></div>
+<div className="vector-3-4"></div>
+<div className="vector-3-5"></div>
+<div className="vector-3-6"></div>
+{/* </div><br /> */}
+{/* <div className="vector-row-4"> */}
+<div className="vector-4-1"></div>
+<div className="vector-4-2"></div>
+<div className="vector-4-3"></div>
+<div className="vector-4-4"></div>
+<div className="vector-4-5"></div>
+<div className="vector-4-6"></div>
+{/* </div> */}
+{/* </div> */}
+
+
         <div className="shape-left-3"></div>
 
-        <div className="dotted-detail">
-
-        <div className="vector-row-1">
-        <div className="vector-1-1"></div>
-        <div className="vector-1-2"></div>
-        <div className="vector-1-3"></div>
-        <div className="vector-1-4"></div>
-        <div className="vector-1-5"></div>
-        <div className="vector-1-6"></div>
-        </div><br />
-
-
-        <div className="vector-row-2">
-        <div className="vector-2-1"></div>
-        <div className="vector-2-2"></div>
-        <div className="vector-2-3"></div>
-        <div className="vector-2-4"></div>
-        <div className="vector-2-5"></div>
-        <div className="vector-2-6"></div>
-        </div><br />
-        <div className="vector-row-3">
-        <div className="vector-3-1"></div>
-        <div className="vector-3-2"></div>
-        <div className="vector-3-3"></div>
-        <div className="vector-3-4"></div>
-        <div className="vector-3-5"></div>
-        <div className="vector-3-6"></div>
-        </div><br />
-        <div className="vector-row-4">
-        <div className="vector-4-1"></div>
-        <div className="vector-4-2"></div>
-        <div className="vector-4-3"></div>
-        <div className="vector-4-4"></div>
-        <div className="vector-4-5"></div>
-        <div className="vector-4-6"></div>
-        </div>
-        </div>
-
-
-        <img src="/images/student.jpg"/>
 
 
 
           <div className="card">
-            <p className="p-find-teacher">Find your </p>
-            <p >best teacher</p><br />
+            <div className="find-your">Find your </div>
+            <div className="best-teacher">best teacher</div><br />
             <text>Whether you are a student trying to find your ideal private language teachers/tutors or a teacher trying to find great students for your customised private lessons!</text>
           </div>
           <input type="search" placeholder="Type here what you are looking for" className="input-search"></input>
